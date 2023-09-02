9c005e2f1e71a8df8989bcedecf1a5dfc142b1a4
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-09-02 15:43:57 +0100

    Try using cache
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+kind create cluster --wait 300s --config /dev/stdin <<EOF
+  kind: Cluster
+  apiVersion: kind.x-k8s.io/v1alpha4
+  containerdConfigPatches:
+    - |-
+      [plugins."io.containerd.grpc.v1.cri".registry.mirrors."docker.io"]
+        endpoint = ["http://192.168.128.1:5000"]
+  EOF
+
+arkade install ingress-nginx
+
+arkade install openfaas
+
+kubectl rollout status -n openfaas deploy/gateway
+
+docker exec kind-control-plane journalctl -u kubelet
